fbebcb17ffbad52591db1593c974ded70ee85a15
adrien-ga <adrien_g@citycable.ch>
2018-09-20 06:03:06 +0000

    Correction of output times reading

--- Simstrat_v2/src/strat_inputfile.f90
+++ Simstrat_v2/src/strat_inputfile.f90
@@ -484,12 +484,10 @@ contains
             call par_file%get('Input.Grid', input_cfg%read_grid_value_from_json, found); call check_field(found, 'Input.Grid', ParName)
          end if
 
-
          call par_file%get('Input.Inflow', QinpName, found); input_cfg%QinpName = QinpName; call check_field(found, 'Input.Inflow', ParName)
          call par_file%get('Input.Outflow', QoutName, found); input_cfg%QoutName = QoutName; call check_field(found, 'Input.Outflow', ParName)
          call par_file%get('Input.Inflow temperature', TinpName, found); input_cfg%TinpName = TinpName; call check_field(found, 'Input.Inflow temperature', ParName)
          call par_file%get('Input.Inflow salinity', SinpName, found); input_cfg%SinpName = SinpName; call check_field(found, 'Input.Inflow salinity', ParName)
-
          ! Path to output folder
          call par_file%get('Output.Path', PathOut, found); output_cfg%PathOut = PathOut; call check_field(found, 'Output.Path', ParName)
          ! Output depth reference
@@ -505,14 +503,12 @@ contains
          ! Treat different input possibilities for output depths
          if (output_cfg%output_depth_type == 7) then ! Path name
             call par_file%get('Output.Depths', zoutName, found); output_cfg%zoutName = zoutName; call check_field(found, 'Output.Depths', ParName)
-         
          else if (output_cfg%output_depth_type == 3) then ! Output depths are given
             call par_file%get('Output.Depths', output_cfg%zout_read, found); call check_field(found, 'Output.Depths', ParName)
             if (output_cfg%output_depth_reference == 'surface') then
                call reverse_in_place(output_cfg%zout_read)
             end if
             output_cfg%depth_interval = 0
-         
          else if (output_cfg%output_depth_type == 5 .or. output_cfg%output_depth_type == 6) then ! Output interval is given
             call par_file%get('Output.Depths', output_cfg%depth_interval, found); call check_field(found, 'Output.Depths', ParName)
          end if
@@ -523,12 +519,10 @@ contains
          ! Treat different input possibilities for output times
          if (output_cfg%output_time_type == 7) then ! Path name
             call par_file%get('Output.Times', toutName, found); output_cfg%toutName = toutName; call check_field(found, 'Output.Times', ParName)
-         
          else if (output_cfg%output_time_type == 3) then ! Output depths are given
             call par_file%get('Output.Times', output_cfg%tout, found); call check_field(found, 'Output.Times', ParName)
             output_cfg%thinning_interval = 0
-         
-         else if (output_cfg%output_time_type == 5) then ! Output interval is given
+         else if (output_cfg%output_time_type == 5 .or. output_cfg%output_time_type == 6) then ! Output interval is given
             call par_file%get('Output.Times', output_cfg%thinning_interval, found); call check_field(found, 'Output.Times', ParName)
             allocate(output_cfg%tout(1))
             output_cfg%tout(1) = output_cfg%thinning_interval
