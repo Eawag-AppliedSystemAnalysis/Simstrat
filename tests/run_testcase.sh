5c47c86d88a0d66db3e797a965c27f4b7fd72735
felmer <franz-josef.elmer@id.ethz.ch>
2019-09-10 08:35:55 +0200

    SSDM-8665: turning existing test into a regression test

--- tests/run_testcase.sh
+++ tests/run_testcase.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+rm TestCases_Results/*.dat
+
 ../build/simstrat TestCase_LakeZurich.par
 
+diff -x .gitignore TestCases_Results_expected/ TestCases_Results/ > diff_results.txt
+if [[ $? == 0 ]]; then
+    echo "====== Successful test ======"
+else
+    echo "====== Test failed ====== see diff_results.txt for more details"
+fi
