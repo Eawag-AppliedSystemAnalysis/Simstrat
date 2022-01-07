25406b6df1b38adf54631d57539e5805fb185251
Fabian Bärenbold <fabian.baerenbold@eawag.ch>
2022-01-07 14:00:33 +0100

    Don't print initial condition by default if output times are given

--- src/strat_outputfile.f90
+++ src/strat_outputfile.f90
@@ -390,48 +390,52 @@ contains
       call output_helper%init(simdata%model%simulation_time, simdata%model%simulation_time_old, self%sim_config%timestep, self%simulation_time_for_next_output, &
                               self%sim_config%start_datum, self%output_config%thinning_interval, &
                               self%output_config%simulation_times_for_output, self%counter)
-      ! Standard display: display when logged: datum, lake surface, T(1), T(surf)
-      if (self%sim_config%disp_simulation == 1 .and. output_helper%write_to_file) then
-         write(6,'(F12.4,F16.4,F20.4,F20.4)') simdata%model%datum, simdata%grid%lake_level, &
-                                              simdata%model%T(simdata%grid%nz_occupied), simdata%model%T(1)
-      ! Extra display: display every iteration: datum, lake surface, T(1), T(surf)
-      else if (self%sim_config%disp_simulation == 2) then
-         write(6,'(F12.4,F20.4,F15.4,F15.4)') simdata%model%datum, simdata%grid%lake_level, &
-                                              simdata%model%T(simdata%grid%ubnd_vol), simdata%model%T(1)
-      end if
-      do i = 1, self%n_vars_Simstrat
-         call output_helper%add_datum(self%output_files(i), "(F12.4)")
-         ! If on volume or faces grid
-         if (self%output_config%output_vars(i)%volume_grid) then
-            ! Interpolate state on volume grid
-            call self%grid%interpolate_from_vol(self%output_config%output_vars(i)%values, self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
-            call output_helper%add_data_array(self%output_files(i), i, self%last_iteration_data, values_on_zout, "(ES14.4E3)")
-         else if (self%output_config%output_vars(i)%face_grid) then
-            ! Interpolate state on face grid
-            call self%grid%interpolate_from_face(self%output_config%output_vars(i)%values, self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
-            call output_helper%add_data_array(self%output_files(i), i, self%last_iteration_data, values_on_zout, "(ES14.4E3)")
-         else
-            call output_helper%add_data_scalar(self%output_files(i), i, self%last_iteration_data, self%output_config%output_vars(i)%values_surf, "(ES14.4E3)")
-         end if
-         call output_helper%next_row(self%output_files(i))
-      end do
 
-      ! AED part
-      if (self%model_config%couple_aed) then
-         do i = self%n_vars_Simstrat + 1, self%n_vars
-            ! Write datum
+      ! Don't print out the initial condition i.e. if counter = 1 (creates problem for automated calibration)
+      if ((self%counter > 1 .and. self%output_config%thinning_interval == 0) .or. self%output_config%thinning_interval /= 0) then
+         ! Standard display: display when logged: datum, lake surface, T(1), T(surf)
+         if (self%sim_config%disp_simulation == 1 .and. output_helper%write_to_file) then
+            write(6,'(F12.4,F16.4,F20.4,F20.4)') simdata%model%datum, simdata%grid%lake_level, &
+                                                 simdata%model%T(simdata%grid%nz_occupied), simdata%model%T(1)
+         ! Extra display: display every iteration: datum, lake surface, T(1), T(surf)
+         else if (self%sim_config%disp_simulation == 2) then
+            write(6,'(F12.4,F20.4,F15.4,F15.4)') simdata%model%datum, simdata%grid%lake_level, &
+                                                 simdata%model%T(simdata%grid%ubnd_vol), simdata%model%T(1)
+         end if
+         do i = 1, self%n_vars_Simstrat
             call output_helper%add_datum(self%output_files(i), "(F12.4)")
-            ! Interpolate state on volume grid
-            if (i < (self%n_vars_Simstrat + self%n_vars_AED_state + 1)) then
-               call self%grid%interpolate_from_vol(self%output_config%output_vars_aed_state%values(:,i - self%n_vars_Simstrat), self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
+            ! If on volume or faces grid
+            if (self%output_config%output_vars(i)%volume_grid) then
+               ! Interpolate state on volume grid
+               call self%grid%interpolate_from_vol(self%output_config%output_vars(i)%values, self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
+               call output_helper%add_data_array(self%output_files(i), i, self%last_iteration_data, values_on_zout, "(ES14.4E3)")
+            else if (self%output_config%output_vars(i)%face_grid) then
+               ! Interpolate state on face grid
+               call self%grid%interpolate_from_face(self%output_config%output_vars(i)%values, self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
+               call output_helper%add_data_array(self%output_files(i), i, self%last_iteration_data, values_on_zout, "(ES14.4E3)")
             else
-               call self%grid%interpolate_from_vol(self%output_config%output_vars_aed_diagnostic%values(:,i - self%n_vars_Simstrat - self%n_vars_AED_state), self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
+               call output_helper%add_data_scalar(self%output_files(i), i, self%last_iteration_data, self%output_config%output_vars(i)%values_surf, "(ES14.4E3)")
             end if
-            ! Write state
-            call output_helper%add_data_array(self%output_files(i), i, self%last_iteration_data, values_on_zout, "(ES14.4E3)")
-            ! Advance to next row
             call output_helper%next_row(self%output_files(i))
          end do
+
+         ! AED part
+         if (self%model_config%couple_aed) then
+            do i = self%n_vars_Simstrat + 1, self%n_vars
+               ! Write datum
+               call output_helper%add_datum(self%output_files(i), "(F12.4)")
+               ! Interpolate state on volume grid
+               if (i < (self%n_vars_Simstrat + self%n_vars_AED_state + 1)) then
+                  call self%grid%interpolate_from_vol(self%output_config%output_vars_aed_state%values(:,i - self%n_vars_Simstrat), self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
+               else
+                  call self%grid%interpolate_from_vol(self%output_config%output_vars_aed_diagnostic%values(:,i - self%n_vars_Simstrat - self%n_vars_AED_state), self%output_config%zout, values_on_zout, self%n_depths, self%output_config%output_depth_reference)
+               end if
+               ! Write state
+               call output_helper%add_data_array(self%output_files(i), i, self%last_iteration_data, values_on_zout, "(ES14.4E3)")
+               ! Advance to next row
+               call output_helper%next_row(self%output_files(i))
+            end do
+         end if
       end if
    end subroutine
 
